f11ae10d8b4762bf4322e435c2d356f055ce73c6
DhanushM20 <20pc12@psgtech.ac.in>
2021-03-30 00:34:21 +0530

    New Output

--- myproject/forum/views.py
+++ myproject/forum/views.py
@@ -7,6 +7,6 @@ def About(request):
     return HttpResponse("<h1>Hello from about</h1>")
 
 def signin (request):
-    return HttpResponse("<h1>Hello from sign in</h1>")
+    return HttpResponse("<h1>Hello from sign in and hello from GIT</h1>")
 
 # Create your views here.
